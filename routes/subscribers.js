ac387d020bd7886da65be92baa45679e1fd51ca0
unknown <shabankhawar99@gmail.com>
2019-12-22 01:40:43 +0500

    3 down 2 to go

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -2,22 +2,62 @@ const express = require("express");
 const router = express.Router();
 const sub = require("../models/subSchema");
 
+//middleware more like topware haha
+const getSub = async (req, res, next) => {
+  let subscriber;
+  try {
+    subscriber = await sub.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json({ message: "Cannot find subscriber" });
+    }
+  } catch (err) {
+    console.log("YO ITS YA BOI THE MIDDLE WARE");
+    res.status(500).json({ message: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+};
+
 //get all
-router.get("/", (req, res) => {
-  console.log("ALL");
+router.get("/", async (req, res) => {
+  // console.log("ALL");
+  try {
+    const subscribers = await sub.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
+
 //get one
-router.get("/:id", (req, res) => {
-  console.log("ONE ", req.params.id);
+router.get("/:id", getSub, (req, res) => {
+  console.log("ONE ", req.params.id, res);
+  res.send(res.subscriber);
 });
+
 //create one
-router.post("/", (req, res) => {
-  console.log("POST ");
+router.post("/", async (req, res) => {
+  // console.log("POST ");
+  // console.log("MITE KUDASAI", req.body);
+  const subscriber = new sub({
+    name: req.body.name,
+    subbedTo: req.body.subbedTo
+  });
+
+  try {
+    const newSub = await subscriber.save();
+    res.status(201).json(newSub);
+  } catch (err) {
+    // console.error(err);
+    res.status(400).json({ message: err.message });
+  }
 });
+
 //update one
 router.patch("/:id", (req, res) => {
   console.log("Patch ", req.params.id);
 });
+
 //delete one
 router.delete("/:id", (req, res) => {
   console.log("Delete ", req.params.id);
